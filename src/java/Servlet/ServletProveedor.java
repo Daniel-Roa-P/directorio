f6b05c47f0a3ac853024c914d3a5380ca9ad77e9
DanielRoa <danbron523@gmail.com>
2018-09-07 17:32:19 -0500

    deje mas entendible los servlet

--- src/java/Servlet/ServletProveedor.java
+++ src/java/Servlet/ServletProveedor.java
@@ -52,77 +52,42 @@ public class ServletProveedor extends HttpServlet {
             out.println("<link type=\"text/css\" rel=\"stylesheet\" href=\"hoja_hola.css\"/>");
             out.println("</head>");
             out.println("<body>");
-            out.println("<form  method=\"get\">\n" +
-"		\n" +
-"		\n" +
-"				\n" +
-"		<h1>Directorio</h1>\n" +
-"	\n" +
-"		<table id=\"t1\" border=3>\n" +
-"			\n" +
-"			<tr>\n" +
-"				\n" +
-"				<td><h1>Nombre</h1></td>\n" +
-"				\n" +
-"				<td><h4>     "+p.getNombre()+"   </h4></td>\n" +
-"\n" +
-"				<td rowspan=\"4\" id=\"img\">\n" +
-"				<img src=\"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT2MdeRuc6u9LB4rL1cewvV-Tug12O7UZ3WpCBNYFXiHpNFqXj4Wg\"></img>\n" +
-"				</td>			\n" +
-"			</tr>\n" +
-"			\n" +
-"			<tr>\n" +
-"				\n" +
-"				<td><h1>Apellido</h1></td>\n" +
-"				\n" +
-"				<td><h4>"+p.getApellido()+"</h4></td>\n" +
-"			\n" +
-"			</tr>\n" +
-"		\n" +
-"					\n" +
-"			<tr>\n" +
-"				\n" +
-"				<td><h1>Direccion</h1></td>\n" +
-"				\n" +
-"				<td><h4>Cll: "+p.getCalle()+" Kr: "+p.getCarrera()+"</h4></td>\n" +
-"			\n" +
-"			</tr>\n" +
-"\n" +
-"\n" +
-"			<tr>\n" +
-"				\n" +
-"				<td><h1>Telefono</h1></td>\n" +
-"				\n" +
-"				<td><h4>"+p.getTelefono()+"</h4></td>\n" +
-"			\n" +
-"			</tr>\n" +
-"\n" +
-"			<tr>\n" +
-"				\n" +
-"				<td><h1>Tipo_contacto</h1></td>\n" +
-"				\n" +
-"				<td><h4>"+p.getTipo_c()+"</h4></td>\n" +
-"				\n" +
-"				<td><h6>Pruducto: "+p.getProducto()+"</h6></td>\n" +
-"\n" +
-"			</tr>\n" +
-"\n" +
-"			<tr>\n" +
-"				<td><h1>Comentario</h1></td>\n" +
-"				\n" +
-"				<td colspan =\"2\"><h4>"+p.getComentario()+"<h4></td>\n" +
-"			</tr>\n" +
-"\n" +
-"			<tr>\n" +
-"				\n" +
-"				<td colspan=\"3\"><a href=\"index.jsp\">Regresar</a></td>\n" +
-"			\n" +
-"			</tr>\n" +
-"		\n" +
-"		</table>	\n" +
-"		\n" +
-"		</form>");
-
+            out.println("<form  method=\"get\">");
+            out.println("<h1>Directorio</h1>");
+            out.println("<table id=\"t1\" border=3>"); 
+            out.println("<tr>");
+            out.println("<td><h1>Nombre</h1></td>");
+            out.println("<td><h4>     "+p.getNombre()+"   </h4></td>");
+            out.println("<td rowspan=\"4\" id=\"img\">");
+            out.println("<img src=\"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT2MdeRuc6u9LB4rL1cewvV-Tug12O7UZ3WpCBNYFXiHpNFqXj4Wg\"></img>");
+            out.println("</td>");
+            out.println("</tr>");
+            out.println("<tr>");
+            out.println("<td><h1>Apellido</h1></td>");
+            out.println("<td><h4>"+p.getApellido()+"</h4></td>");
+            out.println("</tr>");
+            out.println("<tr>");
+            out.println("<td><h1>Direccion</h1></td>");
+            out.println("<td><h4>Cll: "+p.getCalle()+" Kr: "+p.getCarrera()+"</h4></td>");
+            out.println("</tr>");
+            out.println("<tr>");
+            out.println("<td><h1>Telefono</h1></td>");
+            out.println("<td><h4>"+p.getTelefono()+"</h4></td>");
+            out.println("</tr>");
+            out.println("<tr>");
+            out.println("<td><h1>Tipo_contacto</h1></td>");
+            out.println("<td><h4>"+p.getTipo_c()+"</h4></td>");
+            out.println("<td><h6>Pruducto: "+p.getProducto()+"</h6></td>");
+            out.println("</tr>");
+            out.println("<tr>");
+            out.println("<td><h1>Comentario</h1></td>");        
+            out.println("<td colspan =\"2\"><h4>"+p.getComentario()+"<h4></td>");
+            out.println("</tr>");
+            out.println("<tr>");
+            out.println("<td colspan=\"3\"><a href=\"index.jsp\">Regresar</a></td>");
+            out.println("</tr>");
+            out.println("</table>");
+            out.println("</form>");
             out.println("</body>");
             out.println("</html>");
         }
